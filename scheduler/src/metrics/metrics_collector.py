df40b1d6567be7e327e90d94c8253cbd22d88c07
Jungerindy <jona.indy@gmail.com>
2024-10-26 17:08:47 +0200

    skeleton for metrics_collector

--- scheduler/src/metrics/metrics_collector.py
+++ scheduler/src/metrics/metrics_collector.py
@@ -2,37 +2,32 @@ import logging
 import requests
 import sys
 
+def read_current_metric_from_prometheus(metric_name: str):
+    prometheus_url = "http://prometheus-operated.default.svc.cluster.local:9090"
+    try:
+        response = requests.get(
+            f"{prometheus_url}/api/v1/query",
+            params={"query": metric_name},
+        )
+        data = response.json()
+        value = data["data"]["result"][0]["value"][1]
+        return int(value)
+    except Exception as e:
+        logging.error(f"Error, when reading from prometheus: {e}")
+        return None
 
-class MetricsCollector:
-    def __init__(self):
-        pass
+# FIXME
+def get_critical_metrics_for_sf():
+    pass
 
-    def read_metric_from_prometheus(metric_name: str):
-        prometheus_url = "http://prometheus-operated.default.svc.cluster.local:9090"
-        try:
-            response = requests.get(
-                f"{prometheus_url}/api/v1/query",
-                params={"query": metric_name},
-            )
-            data = response.json()
-            value = data["data"]["result"][0]["value"][1]
-            return int(value)
-        except Exception as e:
-            logging.error(f"Error, when reading from prometheus: {e}")
-            return None
+# FIXME
+def get_critical_metrics_for_sl():
+    pass
 
-    # FIXME
-    def get_critical_metrics_for_sf():
-        pass
+# FIXME
+def get_objectives_for_sf():
+    pass
 
-    # FIXME
-    def get_critical_metrics_for_sl():
-        pass
-
-    # FIXME
-    def get_objectives_for_sf():
-        pass
-
-    # FIXME
-    def get_objectives_for_sl():
-        pass
+# FIXME
+def get_objectives_for_sl():
+    pass
