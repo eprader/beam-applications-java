1f7a889401f9920e7bfac123b803e554c4b11d36
Jungerindy <jona.indy@gmail.com>
2024-10-28 18:28:39 +0100

    adapt timeout logic in evaluation monitor class

--- scheduler/src/scheduler_logic/evaluation_monitor.py
+++ scheduler/src/scheduler_logic/evaluation_monitor.py
@@ -21,29 +21,40 @@ class EvaluationMonitor:
         self.evaluation_event = evaluation_event
         self.timeout_duration_sec = timeout_duration_min * 60
         self.sleep_interval = sleep_interval_seconds
+        self.timeout_counter = 0
+        self.periodic_counter = 0
 
     def start_monitoring(self):
         periodic_checks = self.interval_seconds / self.sleep_interval
-        timeout_counter = 0
-        periodic_counter = 1
         while True:
-            metrics = self.collect_metrics()
-            # FIXME
-            # save metrics to db
-            if timeout_counter != 0:
-                timeout_counter -= 1
-
-            if self.check_for_safety_net():
-                if self.evaluate_and_act():
-                    timeout_counter = self.timeout_duration_sec / self.sleep_interval
-            elif timeout_counter == 0 and periodic_counter == periodic_checks:
-                logging.info("Periodical check-up")
-                if self.evaluate_and_act():
-                    timeout_counter = self.timeout_duration_sec / self.sleep_interval
-                periodic_counter = 0
-
-            periodic_counter += 1
+            self.monitor_iteration(periodic_checks)
             time.sleep(self.sleep_interval)
+
+    def monitor_iteration(self, periodic_checks):
+        timeout_counter = self.timeout_counter
+        periodic_counter = self.periodic_counter
+        metrics = self.collect_metrics()
+        # FIXME
+        # save metrics to db
+
+        if self.check_for_safety_net() and timeout_counter == 0:
+            if self.evaluate_and_act():
+                timeout_counter = self.timeout_duration_sec / self.sleep_interval
+        elif periodic_counter >= periodic_checks and timeout_counter == 0:
+            logging.info("Periodical check-up")
+            if self.evaluate_and_act():
+                timeout_counter = self.timeout_duration_sec / self.sleep_interval
+                periodic_counter = 0
+            else:
+                periodic_counter = -1
+
+        if timeout_counter != 0:
+            timeout_counter -= 1
+        else:
+            periodic_counter += 1
+
+        self.timeout_counter = timeout_counter
+        self.periodic_counter = periodic_counter
 
     # FIXME
     def check_for_safety_net(self):
@@ -68,7 +79,9 @@ class EvaluationMonitor:
             return None, None
 
     def evaluate_and_act(self):
-        decision = scheduler_logic.scheduler_logic.run_evaluation(self.running_framework)
+        decision = scheduler_logic.scheduler_logic.run_evaluation(
+            self.running_framework
+        )
         if decision != self.running_framework:
             self.handle_switch(decision)
             return True
