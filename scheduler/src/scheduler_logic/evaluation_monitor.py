8e7b82c36830ff252bfa1732bbf8452f503adcea
Jungerindy <jona.indy@gmail.com>
2024-10-29 18:25:39 +0100

    implement check_safety net for evaluation monitor

--- scheduler/src/scheduler_logic/evaluation_monitor.py
+++ scheduler/src/scheduler_logic/evaluation_monitor.py
@@ -5,15 +5,17 @@ import threading
 import time
 import logging
 import utils.Utils
-
+from datetime import datetime
 
 class EvaluationMonitor:
     def __init__(
         self,
         running_framework: utils.Utils.Framework,
         evaluation_event: threading.Event,
-        application,
-        dataset,
+        application: str,
+        dataset: str,
+        threshold_dict_sf: dict,
+        threshold_dict_sl: dict,
         periodic_checking_min=1,
         timeout_duration_min=10,
         sleep_interval_seconds=30,
@@ -27,6 +29,8 @@ class EvaluationMonitor:
         self.sleep_interval = sleep_interval_seconds
         self.timeout_counter = 0
         self.periodic_counter = 0
+        self.threshold_dict_sf = threshold_dict_sf
+        self.threshold_dict_sl = threshold_dict_sl
 
     def start_monitoring(self):
         periodic_checks = self.interval_seconds / self.sleep_interval
@@ -40,8 +44,8 @@ class EvaluationMonitor:
         metrics = self.collect_metrics()
         # FIXME
         # save metrics to db
-
-        if self.check_for_safety_net() and timeout_counter == 0:
+        database.database_access.insert_scheduler_metrics(datetime.now(),metrics[0], self.running_framework.name)
+        if self.check_for_safety_net(metrics[1]) and timeout_counter == 0:
             if self.evaluate_and_act():
                 timeout_counter = self.timeout_duration_sec / self.sleep_interval
         elif periodic_counter >= periodic_checks and timeout_counter == 0:
@@ -60,10 +64,24 @@ class EvaluationMonitor:
         self.timeout_counter = timeout_counter
         self.periodic_counter = periodic_counter
 
-    # FIXME
-    def check_for_safety_net(self):
-        pass
-        # Return True if there is a safety cause
+    def check_for_safety_net(self, metrics_dic: dict):
+        try:
+            if self.running_framework is utils.Utils.Framework.SF:
+                for metric, value in metrics_dic.items():
+                    if metric == "idleTime":
+                        if value > self.threshold_dict_sf[metric]:
+                            return True
+                    if metric == "busyTime":
+                        if value < self.threshold_dict_sf[metric]:
+                            return True
+            if self.running_framework is utils.Utils.Framework.SL:
+                for metric, value in metrics_dic.items():
+                    if value > self.threshold_dict_sl[metric]:
+                        return True
+        except Exception as e:
+            logging.error(f"Error when accessing safety net: {e}")
+            return False
+        return False
 
     def collect_metrics(self):
         try:
@@ -74,7 +92,7 @@ class EvaluationMonitor:
                 )
             elif self.running_framework is utils.Utils.Framework.SL:
                 return (
-                    metrics.metrics_collector.get_objectives_for_sl(),
+                    metrics.metrics_collector.get_objectives_for_sl("PRED"),
                     metrics.metrics_collector.get_critical_metrics_for_sl(),
                 )
             raise Exception("No valid Framework is given")
@@ -90,13 +108,13 @@ class EvaluationMonitor:
             self.handle_switch(decision)
             return True
         else:
-            database.database_access.store_decision_in_db(self.running_framework)
+            database.database_access.store_decision_in_db(datetime.now(),self.running_framework)
             return False
 
     # FIXME
     def handle_switch(self, decision: utils.Utils.Framework):
         self.evaluation_event.set()
-        database.database_access.store_decision_in_db(decision)
+        database.database_access.store_decision_in_db(datetime.now(),decision)
         while self.evaluation_event.is_set():
             time.sleep(30)
             logging.info("Waiting for event to unset")
