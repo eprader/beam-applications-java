f95a62f501b9f81125f80fc531c710d6a0fb0b5c
Jungerindy <jona.indy@gmail.com>
2024-10-31 21:50:46 +0100

    add tests for change method

--- scheduler/test/test_k8s_service.py
+++ scheduler/test/test_k8s_service.py
@@ -1,13 +1,18 @@
 import pytest
 from unittest.mock import patch, MagicMock, mock_open
 import framework_scheduling.kubernetes_service as k8s_service
+import utils.Utils
 
 
 def test_delete_all_jobs_from_serverful_framework():
-    with patch("framework_scheduling.kubernetes_service.get_jobid_of_running_job", autospec=True,return_value=["job1", "job2"]), \
-         patch("framework_scheduling.kubernetes_service.stop_flink_job",
-               autospec=True) as mock_stop:
-        
+    with patch(
+        "framework_scheduling.kubernetes_service.get_jobid_of_running_job",
+        autospec=True,
+        return_value=["job1", "job2"],
+    ), patch(
+        "framework_scheduling.kubernetes_service.stop_flink_job", autospec=True
+    ) as mock_stop:
+
         k8s_service.delete_all_jobs_from_serverful_framework()
         mock_stop.assert_any_call("flink-session-cluster-rest", "job1")
         mock_stop.assert_any_call("flink-session-cluster-rest", "job2")
@@ -22,20 +27,33 @@ def test_is_flink_deployment_ready():
 
 
 def test_wait_for_flink_deployment():
-    with patch("time.sleep", return_value=None), \
-         patch("framework_scheduling.kubernetes_service.is_flink_deployment_ready", return_value=True,autospec=True):
-        
+    with patch("time.sleep", return_value=None), patch(
+        "framework_scheduling.kubernetes_service.is_flink_deployment_ready",
+        return_value=True,
+        autospec=True,
+    ):
+
         mock_k8s_api = MagicMock()
         assert k8s_service.wait_for_flink_deployment(mock_k8s_api, "flink-app")
 
 
 def test_start_flink_deployment():
     mock_k8s_api = MagicMock()
-    with patch("framework_scheduling.kubernetes_service.client.CustomObjectsApi", return_value=mock_k8s_api,autospec=True), \
-         patch("framework_scheduling.kubernetes_service.wait_for_flink_deployment", return_value=True,autospec=True), \
-         patch("framework_scheduling.kubernetes_service.config.load_incluster_config"):
-        
-        path_manifest = [{"kind": "FlinkDeployment", "metadata": {"name": "test-deployment"}}]
+    with patch(
+        "framework_scheduling.kubernetes_service.client.CustomObjectsApi",
+        return_value=mock_k8s_api,
+        autospec=True,
+    ), patch(
+        "framework_scheduling.kubernetes_service.wait_for_flink_deployment",
+        return_value=True,
+        autospec=True,
+    ), patch(
+        "framework_scheduling.kubernetes_service.config.load_incluster_config"
+    ):
+
+        path_manifest = [
+            {"kind": "FlinkDeployment", "metadata": {"name": "test-deployment"}}
+        ]
         k8s_service.start_flink_deployment(path_manifest)
 
 
@@ -87,15 +105,25 @@ def test_stop_flink_job():
 
 
 def test_start_deployment_and_service():
-    with patch("framework_scheduling.kubernetes_service.client.CoreV1Api"), \
-         patch("framework_scheduling.kubernetes_service.client.AppsV1Api"), \
-         patch("framework_scheduling.kubernetes_service.wait_for_deployment_and_service", autospec=True,return_value=True), \
-         patch("framework_scheduling.kubernetes_service.wait_for_deployment", autospec=True,return_value=True), \
-         patch("framework_scheduling.kubernetes_service.config.load_incluster_config"):
-        
-        path_manifest = [{"kind": "Deployment", "metadata": {"name": "test-deployment"}},
-                         {"kind": "Service", "metadata": {"name": "test-service"}}]
-        
+    with patch("framework_scheduling.kubernetes_service.client.CoreV1Api"), patch(
+        "framework_scheduling.kubernetes_service.client.AppsV1Api"
+    ), patch(
+        "framework_scheduling.kubernetes_service.wait_for_deployment_and_service",
+        autospec=True,
+        return_value=True,
+    ), patch(
+        "framework_scheduling.kubernetes_service.wait_for_deployment",
+        autospec=True,
+        return_value=True,
+    ), patch(
+        "framework_scheduling.kubernetes_service.config.load_incluster_config"
+    ):
+
+        path_manifest = [
+            {"kind": "Deployment", "metadata": {"name": "test-deployment"}},
+            {"kind": "Service", "metadata": {"name": "test-service"}},
+        ]
+
         try:
             k8s_service.start_deployment_and_service(path_manifest)
         except Exception as e:
@@ -103,10 +131,96 @@ def test_start_deployment_and_service():
 
 
 def test_delete_minio():
-    with patch("framework_scheduling.kubernetes_service.terminate_deployment_and_service",autospec=True), \
-         patch("framework_scheduling.kubernetes_service.utils.Utils.read_manifest", autospec=True,return_value=[]):
-        
+    with patch(
+        "framework_scheduling.kubernetes_service.terminate_deployment_and_service",
+        autospec=True,
+    ), patch(
+        "framework_scheduling.kubernetes_service.utils.Utils.read_manifest",
+        autospec=True,
+        return_value=[],
+    ):
+
         try:
             k8s_service.delete_minio()
         except Exception as e:
             pytest.fail(f"delete_minio raised an exception: {e}")
+
+
+@patch("metrics.metrics_collector.get_numRecordsOut", return_value=10)
+@patch("framework_scheduling.kubernetes_service.create_serverful_framework")
+@patch("framework_scheduling.kubernetes_service.create_serverless_framework")
+@patch("framework_scheduling.kubernetes_service.terminate_serverless_framework")
+@patch("framework_scheduling.kubernetes_service.terminate_serverful_framework")
+@patch("time.sleep", return_value=None)  # to avoid actual waiting
+def test_make_change_serverless_to_serverful(
+    mock_sleep,
+    mock_terminate_serverful,
+    mock_terminate_serverless,
+    mock_create_serverless,
+    mock_create_serverful,
+    mock_get_numRecordsOut,
+):
+    framework_used = utils.Utils.Framework.SL
+    number_messages_sent = 5
+    application = "test_app"
+    manifest_docs = "test_manifest"
+    mongodb_address = "test_mongo"
+    dataset = "test_dataset"
+
+    # Execute the function with SL framework
+    k8s_service.make_change(
+        framework_used,
+        number_messages_sent,
+        application,
+        manifest_docs,
+        mongodb_address,
+        dataset,
+    )
+
+    # Assert that termination of serverless and creation of serverful framework are called
+    mock_terminate_serverless.assert_called_once()
+    mock_create_serverful.assert_called_once_with(
+        dataset, manifest_docs, mongodb_address, application
+    )
+    mock_terminate_serverful.assert_not_called()
+    mock_create_serverless.assert_not_called()
+
+
+@patch("metrics.metrics_collector.get_numRecordsOut", return_value=10)
+@patch("framework_scheduling.kubernetes_service.create_serverful_framework")
+@patch("framework_scheduling.kubernetes_service.create_serverless_framework")
+@patch("framework_scheduling.kubernetes_service.terminate_serverless_framework")
+@patch("framework_scheduling.kubernetes_service.terminate_serverful_framework")
+@patch("time.sleep", return_value=None)  # to avoid actual waiting
+def test_make_change_serverful_to_serverless(
+    mock_sleep,
+    mock_terminate_serverful,
+    mock_terminate_serverless,
+    mock_create_serverless,
+    mock_create_serverful,
+    mock_get_numRecordsOut,
+):
+    framework_used = utils.Utils.Framework.SF
+    number_messages_sent = 5
+    application = "test_app"
+    manifest_docs = "test_manifest"
+    mongodb_address = "test_mongo"
+    dataset = "test_dataset"
+
+    # Execute the function with SL framework
+    k8s_service.make_change(
+        framework_used,
+        number_messages_sent,
+        application,
+        manifest_docs,
+        mongodb_address,
+        dataset,
+    )
+
+    # Assert that termination of serverless and creation of serverful framework are called
+    mock_terminate_serverful.assert_called_once_with(manifest_docs)
+    mock_create_serverless.assert_called_once_with(
+        mongodb_address, dataset, application
+    )
+    mock_terminate_serverless.assert_not_called()
+    mock_create_serverful.assert_not_called()
