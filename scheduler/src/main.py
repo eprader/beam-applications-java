c29335f789b8a11dbcbaf8f866e0fc7bd9b1f8b4
Jungerindy <jona.indy@gmail.com>
2024-11-01 21:38:08 +0100

    add threshold dicts to main

--- scheduler/src/main.py
+++ scheduler/src/main.py
@@ -37,15 +37,22 @@ if __name__ == "__main__":
         if not (dataset == "FIT" or dataset == "SYS" or dataset == "TAXI"):
             raise Exception("Unsupported dataset argument")
         path_manifest_flink_session_cluster = (
-            "/app/flink-session-cluster-deployment.yaml"
+            "/app/config_frameworks/flink-session-cluster-deployment.yaml"
         )
         manifest_docs_flink_session_cluster = utils.Utils.read_manifest(
             path_manifest_flink_session_cluster
         )
         framework_used = utils.Utils.Framework.SF
         evaluation_event = threading.Event()
+        threshold_dict_sf = {"idleTime": 900, "busyTime": 100}
+        threshold_dict_sl = {"busyTime": 800, "backPressuredTime": 800}
         evaluation_monitor = scheduler_logic.evaluation_monitor.EvaluationMonitor(
-            framework_used, evaluation_event, application, dataset
+            framework_used,
+            evaluation_event,
+            application,
+            dataset,
+            threshold_dict_sf,
+            threshold_dict_sl,
         )
         framework_scheduler = FrameworkScheduler(framework_used, evaluation_event)
         scheduler_thread = threading.Thread(
@@ -61,7 +68,7 @@ if __name__ == "__main__":
         monitor_thread = threading.Thread(
             target=evaluation_monitor.start_monitoring, name="MetricsMonitorThread"
         )
-
+        
         database.database_access.init_database()
 
         scheduler_thread.start()
