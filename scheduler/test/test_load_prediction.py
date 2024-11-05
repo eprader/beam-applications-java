d8323b77d946e8f20108aa38cf474924ba31d9a7
Jungerindy <jona.indy@gmail.com>
2024-11-05 21:50:22 +0100

    skip test for SARIMAX

--- scheduler/test/test_load_prediction.py
+++ scheduler/test/test_load_prediction.py
@@ -9,14 +9,14 @@ def sample_data():
     data = pd.Series(range(100), index=date_range)
     return data
 
-
+@pytest.mark.skip(reason="SARIMAX method have been removed")
 def test_load_predictor_model_creation_SARIMAX(sample_data):
     predictor = LoadPredictor()
     model = predictor.make_model_sarimax(sample_data)
     assert model is not None, "SARIMAX model creation failed."
 
 
-@pytest.mark.skip(reason="Skipping SARIMAX model creation test for now")
+@pytest.mark.skip(reason="SARIMAX method have been removed")
 def test_load_predictor_predictions_SARIMAX(sample_data):
     predictor = LoadPredictor()
     model = predictor.make_model_sarimax(sample_data)
