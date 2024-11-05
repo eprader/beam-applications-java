e2872bb68745e7f6630bd21010c9e8cec289c0f3
Jungerindy <jona.indy@gmail.com>
2024-11-05 18:53:47 +0100

    fix formatting

--- scheduler/test/test_load_prediction.py
+++ scheduler/test/test_load_prediction.py
@@ -2,11 +2,13 @@ import pytest
 import pandas as pd
 from timeseriesPredictor.LoadPredictor import LoadPredictor
 
+
 @pytest.fixture
 def sample_data():
     date_range = pd.date_range(start="2023-01-01", periods=100, freq="30s")
     data = pd.Series(range(100), index=date_range)
     return data
+
 
 def test_load_predictor_model_creation_SARIMAX(sample_data):
     predictor = LoadPredictor()
@@ -21,15 +23,15 @@ def test_load_predictor_predictions_SARIMAX(sample_data):
     predictions = predictor.make_predictions_sarimax(model, forecast_periods=5)
     assert len(predictions) == 5, "Prediction generation failed; expected 5 values."
 
+
 def test_load_predictor_model_creation_ARIMA(sample_data):
     predictor = LoadPredictor()
     model = predictor.make_model_arima(sample_data)
     assert model is not None, "SARIMAX model creation failed."
 
+
 def test_load_predictor_predictions_ARIMA(sample_data):
     predictor = LoadPredictor()
     model = predictor.make_model_arima(sample_data)
-    predictions = predictor.make_predictions_arima(forecast_periods=5)
+    predictions = predictor.make_predictions_arima(5)
     assert len(predictions) == 5, "Prediction generation failed; expected 5 values."
-
-
